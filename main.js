fe3ccbd9336c5dbc409b3b4b19128b3ce7ff0413
faizan-141 <faizanfarooq141@gmail.com>
2024-01-11 21:35:17 +0530

    made somewhat responsive

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const accordionBtns = document.querySelectorAll('.nf-qa-btn');
 
 function closeOpenAccords(curElement) {
-    const accordionList = document.querySelectorAll(".nf-qa-answer[data-is-open]");
+    const accordionList = document.querySelectorAll(".nf-qa-answer[data-is-open=\"true\"");
 
     for (const accordionDiv of accordionList) {
         const isOpen = accordionDiv.dataset.isOpen;
+
+        // if (event.target.classList) {
+            
+        // }
 
         // if accordion is open other then one already open then close it 
         if (isOpen === "true" && accordionDiv !== curElement) {
@@ -18,8 +22,17 @@ accordionBtns.forEach((accordionBtn) => {
     accordionBtn.addEventListener('click', (event) => {
         const answerDiv = event.currentTarget.querySelector('.nf-qa-answer');
         
+        // console.log(event.target);
+        // console.log();
+        // Need Fix
+        if (event.target.classList[0] === "nf-qa-answer") {
+            return
+        }
+
         closeOpenAccords(answerDiv);
         
+        
+
         answerDiv.dataset.isOpen = answerDiv.dataset.isOpen === "true" ? "false" : "true";   
     })
 });
